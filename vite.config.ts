b08e3849be34309329041f96765c54490cfbed34
hamayou-61 <caster2596@gmail.com>
2024-04-22 17:26:53 +0900

    viteのconfigファイル追加

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+// nuxtには不要だがstorybook用に必要
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+})
