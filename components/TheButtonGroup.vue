b92fc987954f9664483eb3ca1b8378816615cf03
hamayou-61 <caster2596@gmail.com>
2024-04-22 17:13:11 +0900

    buttonGroupコンポーネントの追加

---
+++ components/TheButtonGroup.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-btn-group density="compact" class="mx-2">
+    <v-btn
+      variant="tonal"
+      class="font-weight-bold"
+      prepend-icon="mdi-file-outline"
+      color="secondary"
+      data-testid=""
+    >
+      テストです。
+      <v-badge
+        v-if="isNew"
+        color="warning"
+        content="NEW"
+        inline
+      />
+    </v-btn>
+    <v-divider vertical />
+    <v-menu :close-on-content-click="false">
+      <template v-slot:activator="{ props }">
+        <v-btn
+          variant="tonal"
+          v-bind="props"
+          size="x-small"
+          color="secondary"
+          data-testid="new-order-chevron-down"
+        >
+
+          <v-icon size="x-large" icon="$chevronDown" />
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item link>
+          <v-list-item-title  data-testid="ci-medical-new-order-button">
+            リストページ
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-btn-group>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  /**
+   * Newバッジ
+   */
+  isNew?: boolean
+}>()
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
+
   <p>テスト<br>テストテストテステスト<br>テストテストテストテスト<br>テストテストテストテスト<br>テストテストテストト</p>
+  <the-button-group :isNew="false"></the-button-group>
 </template>
 
 <script setup>
